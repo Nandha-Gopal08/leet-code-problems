1adf8fda4a7c718d5ea1f437f783325ced9bd705
Nandha Gopal <nandhagopal182008@gmail.com>
2026-07-18 16:42:19 +0530

    LeetCode #217 Problem

---
+++ p217 duplicates.py
@@ -0,0 +1,9 @@
+def duplicate(nums):
+    seen=set()
+    for num in nums:
+        if num in seen:
+            return True
+        seen.add(num)
+    return False
+nums=[1,2,3,1,2]
+print(duplicate(nums))
