f8a33d5b7d4ab1613d6c46c08ffba664e540462f
Nandha Gopal <nandhagopal182008@gmail.com>
2026-07-18 16:43:46 +0530

    LeetCode #189 Problem

---
+++ p189 rotate array.py
@@ -0,0 +1,16 @@
+def rotate(nums,k):
+    n=len(nums)
+    k=k%n
+    def reverse(nums,left,right):
+        while(left<right):
+            nums[left],nums[right]=nums[right],nums[left]
+            left+=1
+            right-=1
+    reverse(nums,0,n-1)
+    reverse(nums,0,k-1)
+    reverse(nums,k,n-1)
+    return nums
+k=int(input("enter k"))
+
+nums=[1,2,3,4,5,6,7]
+print(rotate(nums,k))
