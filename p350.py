0884da2d119fbe72b6a3e339d1e98b8da10cc16e
Nandha Gopal <nandhagopal182008@gmail.com>
2026-07-22 20:41:06 +0530

    LeetCode #350 Intersection of Two Arrays II

---
+++ p350.py
@@ -0,0 +1,16 @@
+def intersection(nums1,nums2):
+    count={}
+    result=[]
+    for num in nums1:
+        if num in count:
+            count[num]+=1
+        else:
+            count[num]=1
+    for num in nums2:
+        if num in count and count[num]>0:
+            result.append(num)
+            count[num]-=1
+    return result
+nums1=[4,9,5]
+nums2=[9,4,9,8,4]
+print(intersection(nums1,nums2))
