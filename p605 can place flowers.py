3e6681d686e58a1bdc3639a2a51cc46f69bca00d
Nandha Gopal <nandhagopal182008@gmail.com>
2026-07-28 18:55:00 +0530

    LeetCode #605 Can Place Flowers

---
+++ p605 can place flowers.py
@@ -0,0 +1,16 @@
+def plant(flowerbed,n):
+    for i in range(len(flowerbed)):
+        if n==0:
+            return True
+        if flowerbed[i]==0:
+            left=(i==0 or flowerbed[i-1]==0)
+            right=(i==len(flowerbed)-1 or flowerbed[i+1]==0)
+            if left and right:
+                flowerbed[i]=1
+                n-=1
+        if(n==0):
+            return True
+    return False
+flowerbed=[1]
+n=int(input("ENTER PLANTS"))
+print(plant(flowerbed,n))
