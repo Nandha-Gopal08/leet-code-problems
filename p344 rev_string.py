fbce4749b399894599f048962b8fa357dbb77b7e
Nandha Gopal <nandhagopal182008@gmail.com>
2026-08-05 18:28:16 +0530

    LeetCode #344 Reverse The String

---
+++ p344 rev_string.py
@@ -0,0 +1,10 @@
+def rev_str(s):
+    left=0
+    right=len(s)-1
+    while(left<right):
+        s[left],s[right]=s[right],s[left]
+        left+=1
+        right-=1
+    return s
+s = ["h","e","l","l","o"]
+print(rev_str(s))
