7b7b2b07bd728bf891c768f2d7b93eac5e65894f
Nandha Gopal <nandhagopal182008@gmail.com>
2026-08-05 19:05:48 +0530

    LeetCode #941 Valid Mountain array

---
+++ p941 mountain.py
@@ -0,0 +1,14 @@
+def mount(arr):
+    n=len(arr)
+    if(n<3):
+        return False
+    i=0
+    while(i+1<n and arr[i]<arr[i+1]):
+        i+=1
+    if(i==0 or i==n-1):
+        return False
+    while(i+1<n and arr[i]>arr[i+1]):
+        i+=1
+    return i==n-1
+arr = [0,1,2,3,4,5,2,3,5,8,9]
+print(mount(arr))
