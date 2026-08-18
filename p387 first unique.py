d82a9a2cd35a1c7288ab816055ebe587848dfcf0
Nandha Gopal <nandhagopal182008@gmail.com>
2026-08-18 17:09:58 +0530

    LeetCode #387 First Unique Character In String

---
+++ p387 first unique.py
@@ -0,0 +1,13 @@
+s = "hello"
+def firstunique(s):
+    freq={}
+    for ch in s:
+        if ch in freq:
+            freq[ch]+=1
+        else:
+            freq[ch]=1
+    for i,ch in enumerate(s):
+        if freq[ch]==1:
+            return i
+    return -1
+print(firstunique(s))
