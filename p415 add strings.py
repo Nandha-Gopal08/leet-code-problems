c1df2335c4c47e32ed64130ed8eb2da46211f40e
Nandha Gopal <nandhagopal182008@gmail.com>
2026-08-24 17:18:11 +0530

    LeetCode #415 Add Strings

---
+++ p415 add strings.py
@@ -0,0 +1,19 @@
+def add_strings(num1,num2):
+    i=len(num1)-1
+    j=len(num2)-1
+    carry=0
+    res=[]
+    while i>=0 or j>=0 or carry:
+        digit1=ord(num1[i])-ord('0') if i>=0 else 0
+        digit2=ord(num2[j])-ord('0') if j>=0 else 0
+        total=digit1+digit2+carry
+        res.append(chr(total % 10 + ord('0')))
+        carry=total//10
+        i -= 1
+        j -= 1
+    return "".join(res[::-1])
+        
+        
+num1 = "11"
+num2 = "123"
+print(add_strings(num1,num2))
