7391d61f280b572e80b6fcae9a136616a084f2c8
Nandha Gopal <nandhagopal182008@gmail.com>
2026-08-25 21:38:21 +0530

    LeetCode #392 Is Subsequence

---
+++ p392 si subsequence.py
@@ -0,0 +1,9 @@
+def subsequence(s,t):
+    j = 0
+    for i in range(len(t)):
+        if j < len(s) and s[j]== t[i]:
+            j += 1
+    return j == len(s)
+s = "abc"
+t = "ahbgdc"
+print(subsequence(s,t))
