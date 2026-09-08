621bd75572bb60dacfa13a9b15761e61b5f0c94f
Nandha Gopal <nandhagopal182008@gmail.com>
2026-09-08 20:23:01 +0530

    LeetCode #925 Long Pressed Name

---
+++ p925 long pressed name.py
@@ -0,0 +1,12 @@
+def long_press_name(name,typed):
+    i = 0
+    for j in range(len(typed)):
+        if i < len(name) and name[i] == typed[j]:
+            i += 1
+        elif j == 0 or typed[j] != typed[j-1]:
+            return False
+    return i == len(name)
+name = "alex"
+typed = "aaleex"
+
+print(long_press_name(name,typed))
